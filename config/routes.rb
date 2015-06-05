56088c38c551ef42c3dee72881ca61c6d768e29e
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-05 01:43:40 +0000

    generated static pages home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'home/about'
+
   resources :movies
 
   resources :actors
 
   resources :genres
   
-  root 'movies#index'
+  # root 'movies#index'
+  
+root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
