072de0635b8e769fd0ff22b3f00e626caccbd42f
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-09 20:43:29 +0000

    fixed carrierwave uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609194701) do
+ActiveRecord::Schema.define(version: 20150609204216) do
 
   create_table "actors", force: true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150609194701) do
     t.integer  "actor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "cover"
   end
 
   create_table "users", force: true do |t|
